7d41b87df52a34861e80f48f1c51fca2f620bf3a
maty <mfedak@gmail.com>
2015-11-22 17:14:22 +0100

    added vendors

--- app/vendors.js
+++ app/vendors.js
@@ -27,6 +27,45 @@ export const vendors_map = {
     name: "Github",
     logo: "icons/github.svg",
     reloadUrl: "*://*.github.com/*"
-  }
-  
+  },
+  dropbox: {
+    cookie: {
+      domain: ".www.dropbox.com",
+      name: "lid"
+    },
+    name: "Dropbox",
+    logo: "icons/dropbox.svg",
+    reloadUrl: "*://*.dropbox.com/*",
+    detailUrl: "https://www.dropbox.com/account#security"
+  },
+  linkedin: {
+    cookie: {
+      domain: ".www.linkedin.com",
+      name: "li_at"
+    },
+    name: "",
+    logo: "icons/linkedin.svg",
+    reloadUrl: "*://*.linkedin.com/*",
+    detailUrl: "https://www.linkedin.com/settings/sessions"
+  },
+  twitter: {
+    cookie: {
+      domain: ".twitter.com",
+      name: "auth_token"
+    },
+    name: "Twitter",
+    logo: "icons/twitter.svg",
+    reloadUrl: "*://*.twitter.com/*",
+    detailUrl: "https://twitter.com/settings/applications"
+  },
+  spotify: {
+    cookie: {
+      domain: ".spotify.com",
+      name: "sp_dc"
+    },
+    name: "",
+    logo: "icons/spotify.png",
+    reloadUrl: "*://*.spotify.com/*",
+    detailUrl: "https://www.spotify.com/my-en/account/overview/"
+  },
 };
